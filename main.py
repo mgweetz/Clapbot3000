be012dcccc5401c44de26d354cf17dbcb51ac852
mgweetz <mgweets@gmail.com>
2025-06-25 16:58:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ def home():
     return "🤖 ClapBot3000 is online and roasting humans."
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
 
 def keep_alive():
     t = Thread(target=run)
